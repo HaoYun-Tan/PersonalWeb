d3ae3c5fa9f86ae66963e57667740a47665abfca
Jin Qin <qin_jin@live.com>
2021-08-12 20:02:13 -0700

    update: add toast for new article

--- client/src/pages/NewArticle.tsx
+++ client/src/pages/NewArticle.tsx
@@ -1,10 +1,19 @@
 import React, { useState } from 'react'
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, Toast } from 'react-bootstrap';
 import './NewArticle.css'
+
+enum ToastBody {
+    TOAST_SUCCEED = 'Sucessfully created an article!',
+    TOAST_FAILED = 'Failed created an article!'
+}
 
 function NewArticle() {
     const [articleTitle, setArticleTitle] = useState<string>('')
     const [articleText, setArticleText] = useState<string>('')
+
+    const [showToast, setShowToast] = useState(false);
+    const hideToast = () => setShowToast(false);
+    const [toastBody, setToastBody] = useState<string>(ToastBody.TOAST_SUCCEED);
     return (
         <div className="NewArticleLayout">
             <Form>
@@ -13,19 +22,27 @@ function NewArticle() {
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formArticleText">
-                    <Form.Control as="textarea" rows={30} placeholder="Article text" onChange={e => setArticleText(e.target.value)} />
+                    <Form.Control as="textarea" rows={10} placeholder="Article text" onChange={e => setArticleText(e.target.value)} />
                 </Form.Group>
 
                 <div className="NewArticleLayout-BtnCreateArticle">
                     <Button 
                         variant="primary"
                         size='lg' 
-                        onClick={e => onCreateArticle(e, {articleTitle, articleText})}
+                        onClick={e => onCreateArticle(e, {articleTitle, articleText, setShowToast, setToastBody})}
                     >
                         CREATE ARTICLE
                     </Button>
                 </div>
             </Form>
+
+            <Toast className='NewArticle-Toast' show={showToast} onClose={hideToast}>
+                <Toast.Header>
+                    <strong className="me-auto">Create Article</strong>
+                    <small>11 mins ago</small>
+                </Toast.Header>
+                <Toast.Body>{toastBody}</Toast.Body>
+            </Toast>
         </div>
     );
 }
@@ -34,28 +51,42 @@ export default NewArticle;
 
 interface CreateArticleProps {
     articleTitle: string,
-    articleText: string
+    articleText: string,
+    setShowToast: any,
+    setToastBody: any
 }
 
 async function onCreateArticle(e: React.MouseEvent<HTMLElement, MouseEvent>, props: CreateArticleProps) {
     e.preventDefault();
 
+    const {articleTitle, articleText, setShowToast, setToastBody} = props;
+
     const req_options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
-            title: props.articleTitle,
-            text: props.articleText,
+            title: articleTitle,
+            text: articleText,
             user_id: 1
         })
     }
 
-    fetch('http://localhost:8868/api/article', req_options)
-        .then(rsp => rsp.json())
-        .then(data => {
-            
+    fetch('http://localhost:8080/api/article', req_options)
+        .then(rsp => { 
+            if (rsp.status == 201) {
+                setShowToast(true);
+                setToastBody(ToastBody.TOAST_SUCCEED);
+            } else {
+                // faile to create
+                setShowToast(true);
+                setToastBody(ToastBody.TOAST_FAILED);
+            }
         })
         .catch(e => {
+            // exception
             console.log(e)
+
+            setShowToast(true);
+            setToastBody(ToastBody.TOAST_FAILED);
         })
 }
