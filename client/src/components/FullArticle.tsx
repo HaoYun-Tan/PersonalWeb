13b8ba102232f4ef9f3ba37d578670b9236cffa7
HaoYun-Tan <972901605@qq.com>
2021-08-24 09:35:19 +0800

    Co-authored-by: Jin.Qin <qin_jin@live.com>

--- client/src/components/FullArticle.tsx
+++ client/src/components/FullArticle.tsx
@@ -1,18 +1,33 @@
 import "./FullArticle.css"
-import {useState, useLayoutEffect, useRef } from 'react'
+import {useState, useEffect, useLayoutEffect, useRef } from 'react'
 import CloseBtnImg from '../assets/img/CloseButton.png';
-
+import Comments from "./Comments";
 interface FullArticleData {
     id: number
-    title: string,
-    text: string,
-    created_at: string,
-    updated_at: string,
     setEventShowFullArticle: any
 }
 
+interface ArticleData {
+    id: number,
+    title: string,
+    text: string,
+    createdAt: string,
+    updatedAt: string
+}
+
+const emptyArticleData = (): ArticleData => ({
+    id: -1,
+    title: '',
+    text: '',
+    createdAt: '',
+    updatedAt: ''
+})
+
 function FullArticle(props: FullArticleData){
-    const { id, title, text, created_at, updated_at, setEventShowFullArticle} = props;
+    const { id, setEventShowFullArticle} = props;
+    const [articleData, setArticleData] = useState<ArticleData>(emptyArticleData)
+    const [hasPermission, setHasPermission] = useState<boolean>(false)
+
     const selfRef = useRef<HTMLInputElement>(null)
     useLayoutEffect(() => {
         if (selfRef.current) {
@@ -20,6 +35,38 @@ function FullArticle(props: FullArticleData){
             selfRef.current.style.left = `${computedLeft}px`
         }
     }, [])
+
+    useEffect(() => {
+        const user_id = 1
+        fetch(`http://localhost:8080/api/article?article_id=${id}&user_id=${user_id}`)
+            .then(rsp => {
+                if (rsp.status === 200) {
+                    setHasPermission(true)
+                    return rsp.json()
+                } else {
+                    setHasPermission(false)
+                    throw Error('no permission')
+                }
+            })
+            .then(json => {
+                const article_data = json.data
+
+                setArticleData(
+                    {
+                        id: article_data.ID, 
+                        title: article_data.Title, 
+                        text: article_data.Text, 
+                        createdAt: article_data.CreatedAt, 
+                        updatedAt: article_data.UpdatedAt
+                    }
+                )
+            })
+            .catch(e => {
+                console.log(e)
+            })
+        
+        console.log(`requested article (id: ${id}) full data`)
+    }, [id])
 
     return(
         <div ref={selfRef} className = "FullArticle">
@@ -34,16 +81,37 @@ function FullArticle(props: FullArticleData){
                     src={CloseBtnImg}
                     className="FullArticle_CloseBtn"
                     onClick={() => {
-                        setEventShowFullArticle({id: 0, title: '', text: '', created_at: '', updated_at: '', show: false})
+                        setEventShowFullArticle({id: 0, show: false})
                     }}
                 />
             </div>
-            <div className  = "FullArticle_title">
-                {props.title}
-            </div>
-            <div className = "FullArticle_text">
-                {props.text}
-            </div>
+            
+            {
+            hasPermission ? 
+            (
+                <>
+                <div className  = "FullArticle_title">
+                    {articleData.title}
+                </div>
+                <div className = "FullArticle_text">
+                    {articleData.text}
+                </div>
+
+                <div>
+                    ------
+                </div>
+
+                <Comments articleId={id}/>
+                </>
+            )
+            :
+            (
+                <div>
+                    No Permission
+                </div>)
+            }
+
+            
         </div>
     );
 }
